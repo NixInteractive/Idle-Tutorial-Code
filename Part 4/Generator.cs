912e2de05c935471b6a7f4655e5ef68afcad424c
NixInteractive <nixinteractive@yahoo.com>
2023-11-25 03:22:19 -0500

    Create Generator.cs

---
+++ Part 4/Generator.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using BreakInfinity;
+using UnityEngine.UI;
+
+public class Generator : Monobehaviour
+{
+    public GameManager GM;
+
+    public Image progressBar;
+    
+    public TextMeshProUGUI nameDisp;
+    public TextMeshProUGUI quantityDisp;
+    public TextMeshProUGUI incomeDisp;
+
+    public BigDouble quantity;
+    public BigDouble income;
+
+    public float maxTime;
+    public float time;
+
+    public string generatorName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        quantityDisp.text = GM.SciNotToUSName(quantity);
+        incomeDisp.text = GM.SciNotToUSName(income);
+        nameDisp.text = generatorName;
+
+        time += Time.deltaTime;
+
+        if(time >= maxTime)
+        {
+            GM.money += income;
+            time = 0;
+        }
+
+        progressBar.fillAmount = time / maxTime;
+    }
+}
