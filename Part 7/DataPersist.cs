69cf7be6cca729a5e0966cc5857cc87cb32b20ac
NixInteractive <nixinteractive@yahoo.com>
2023-12-10 21:27:50 -0500

    Create DataPersist.cs

---
+++ Part 7/DataPersist.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataPersist : MonoBehaviour
+{
+    string path;
+
+    public GameData GD = new GameData();
+
+    private void Awake()
+    {
+        path = Application.persistentDataPath + "/data.json";
+    }
+
+    public bool TryLoad()
+    {
+        return File.Exists(path);
+    }
+
+    public void LoadData()
+    {
+        if (TryLoad())
+        {
+            string saveData = File.ReadAllText(path);
+
+            GD = JsonUtility.FromJson<GameData>(saveData);
+        }
+    }
+
+    public void SaveData()
+    {
+        string saveData = JsonUtility.ToJson(GD);
+
+        File.WriteAllText(path, saveData);
+    }
+}
