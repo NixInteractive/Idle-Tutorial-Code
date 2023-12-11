a487f90506984c153d434cdd7857732058e73e11
NixInteractive <nixinteractive@yahoo.com>
2023-12-10 23:44:18 -0500

    Create GameData.cs

---
+++ Part 8/GameData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BreakInfinity;
+using System;
+
+[System.Serializable]
+public class GameData
+{
+    public BigDouble money;
+    public BigDouble gen1;
+    public BigDouble gen2;
+    public BigDouble gen3;
+
+    public DateTime saveTime;
+}
